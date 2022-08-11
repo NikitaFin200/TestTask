16dd7ee356ea71661ac9f9c6422aca475aff4547
Никита <nik.findyurov@mail.ru>
2022-08-12 04:28:20 +0700

    new commit

--- MergeSorting/src/myFile/classes_whith_methods/writeFile.java
+++ MergeSorting/src/myFile/classes_whith_methods/writeFile.java
@@ -1,12 +1,10 @@
 package myFile.classes_whith_methods;
 
 import java.io.*;
-import java.util.Scanner;
 import java.util.Vector;
 
 public class writeFile {
     public static void write(String name, Vector<String> vector) throws IOException {
-        Scanner scanner = new Scanner(System.in);
         File file = new File(name);
         FileWriter fileWriter = new FileWriter(file);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
--- MergeSorting/src/myFile/mergeSorting/MergeArray.java
+++ MergeSorting/src/myFile/mergeSorting/MergeArray.java
@@ -1,28 +1,8 @@
 package myFile.mergeSorting;
 
-import java.util.Arrays;
-
 public class MergeArray {
     public static int[] merge(int[] mas1, int[] mas2, int[] mas3, int count) {
         int[] newArray = new int[mas1.length + mas2.length];
-
-/*
-        for (int i = 0; i < newArray.length; i++) {
-            if (positionA == newMas1.length) {
-                newArray[i] = newMas2[i - positionB];
-                positionB++;
-            } else if (positionB == newMas2.length) {
-                newArray[i] = newMas1[i - positionA];
-                positionA++;
-            } //else if (positionC == mas.)
-            else if (newMas1[i - positionA] < newMas2[i - positionB]) {
-                newArray[i] = newMas1[i - positionA];
-                positionB++;
-            } else {
-                newArray[i] = newMas2[i - positionB];
-                positionA++;
-            }
-        }*/
 
         int i = 0, j = 0;
 
