b7766fc77138ec6db9e79f37c5d9c8b54cb0ca0e
Nikita <nik.findyruv@mail.ru>
2022-08-12 03:17:55 +0700

    rename all and send

--- MergeSorting/src/myFile/classes_whith_methods/writeFile.java
+++ MergeSorting/src/myFile/classes_whith_methods/writeFile.java
@@ -5,26 +5,9 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class writeFile {
-    public static void write(String name) throws IOException {
+    public static void write(String name, Vector<String> vector) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        Vector<String> vector = new Vector<>();
         File file = new File(name);
-        int number;
-
-        int command = 2;
-
-        while (command != 1) {
-            if (command == 2) {
-                System.out.println("Please, enter number:");
-                number = scanner.nextInt();
-                vector.add(String.valueOf(number));
-            }
-
-            System.out.println("Please, enter command:");
-            command = scanner.nextInt();
-        }
-
-        //System.out.println(vector);
         FileWriter fileWriter = new FileWriter(file);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
--- MergeSorting/src/myFile/main/Main.java
+++ MergeSorting/src/myFile/main/Main.java
@@ -6,51 +6,40 @@ import myFile.mergeSorting.MergeArray;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        /*String nam = "in1.txt";
-        File file = new File(nam);
-        String[] arrayFiles = {"in1.txt", "in2.txt", "in3.txt"};
-
-
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write("number");
-        fileWriter.write(9);
-        fileWriter.flush();
-
-        FileReader fileReader = new FileReader("in1.txt");
-
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        String[] arr = {"hello", "nt"};
-        bufferedWriter.write(arr[0]);
-        bufferedWriter.newLine();
-        bufferedWriter.write(arr[1]);
-        bufferedWriter.flush();
-        bufferedWriter.close();
-
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String[] mas = new String[3];
-        while (bufferedReader.ready()) {
-            mas[0] = bufferedReader.readLine();
-            mas[1] = bufferedReader.readLine();
-            mas[2] = bufferedReader.readLine();
-        }
-
-        System.out.println(Arrays.toString(mas));*/
-
+        Vector<String> vec = new Vector<>();
         String[] array = {"in1.txt", "in2.txt", "in3.txt"};
         String[] mas1 = new String[1];
         String[] mas2 = new String[1];
         String[] mas3 = new String[1];
 
-        for (int i = 0; i < array.length; i++) {
-            writeFile.write(array[i]);
-            System.out.println(Arrays.toString(new Vector[]{readFile.read(array[i])}));
-        }
+        int number;
+        int counter = 0;
+
+        do {
+            Vector<String> vector = new Vector<>();
+            int command = 2;
+            while (command != 1) {
+                if (command == 2) {
+                    System.out.println("Please, enter number:");
+                    number = scanner.nextInt();
+                    vector.add(String.valueOf(number));
+                }
+
+                System.out.println("Please, enter command:");
+                command = scanner.nextInt();
+            }
+
+            writeFile.write(array[counter], vector);
+            System.out.println(Arrays.toString(new Vector[]{readFile.read(array[counter])}));
+            counter++;
+        } while (counter != array.length);
 
         mas1 = readFile.read(array[0]).toArray(mas1);
         mas2 = readFile.read(array[1]).toArray(mas2);
@@ -75,6 +64,15 @@ public class Main {
         }
 
         int count = 2;
-        System.out.println(Arrays.toString(MergeArray.merge(newMas1, newMas2, newMas3,count)));
+        System.out.println(Arrays.toString(MergeArray.merge(newMas1, newMas2, newMas3, count)));
+        int[] y = MergeArray.merge(newMas1, newMas2, newMas3, count);
+        String[] fin = new String[y.length];
+
+        for (int i = 0; i < y.length; i++) {
+            fin[i] = String.valueOf(y[i]);
+        }
+
+        Collections.addAll(vec, fin);
+        writeFile.write("final.txt", vec);
     }
 }
